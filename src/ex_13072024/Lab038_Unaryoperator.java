9182f6339222566f39805fca01052b1d56476a89
padma <bubloo@27>
2024-07-17 15:55:22 +0200

    All Progams of July 13 class added

---
+++ src/ex_13072024/Lab038_Unaryoperator.java
@@ -0,0 +1,13 @@
+package ex_13072024;
+
+public class Lab038_Unaryoperator {
+    public static void main(String[] args) {
+        //unary operator:single operand.
+
+        int age = +65;
+        int lambo = -1;
+        System.out.println(age);
+        System.out.println(lambo+2);
+
+    }
+}
---
+++ src/ex_13072024/Lab046_Logicalnot.java
@@ -0,0 +1,12 @@
+package ex_13072024;
+
+public class Lab046_Logicalnot {
+    public static void main(String[] args) {
+        //relational not reverse the result of relational true or false.ie if result is true it will change false due to not condition.
+        boolean a  = true;
+        System.out.println(!a);
+        System.out.println(!(10<20));
+        System.out.println((!!!!!(10>20)));
+
+    }
+}
