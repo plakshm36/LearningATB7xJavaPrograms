e587288009b98003954408cd9748f5e69775a8b5
padma <bubloo@27>
2024-07-19 15:14:48 +0200

    All Progams of July 13 class added

---
+++ src/ex_14072024/Lab071_Scpheaex2.java
@@ -0,0 +1,14 @@
+package ex_14072024;
+
+public class Lab071_Scpheaex2 {
+    public static void main(String[] args) {
+        String s1 = "Pramod";
+        System.out.println(s1);
+        String s2 = s1.toLowerCase();
+        System.out.println(s2);
+        System.out.println(s1);
+        // 2 - Pramod, pramod - SCP
+    }
+
+
+}
---
+++ src/ex_14072024/Lab081_Mathfunc.java
@@ -0,0 +1,12 @@
+package ex_14072024;
+
+public class Lab081_Mathfunc {
+    public static void main(String[] args) {
+        double sq = Math.sqrt(2);
+        int result = Math.max(10,23);
+        double b = Math.toDegrees(result);
+        System.out.println(sq);
+        System.out.println(result);
+        System.out.println(b);
+    }
+}
