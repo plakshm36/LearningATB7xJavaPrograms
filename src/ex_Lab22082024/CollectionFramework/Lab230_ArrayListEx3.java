8d908be3f851716a222326aa9c56391e08cfadca
padma <bubloo@27>
2024-08-22 19:06:11 +0200

    All Progams of Aug 18 class added

---
+++ src/ex_Lab22082024/CollectionFramework/Lab230_ArrayListEx3.java
@@ -0,0 +1,16 @@
+package ex_Lab22082024.CollectionFramework;
+
+import java.util.List;
+
+public class Lab230_ArrayListEx3 {
+    public static void main(String[] args) {
+        List shopping_list = List.of("Milk","Bread","Butter","Cheese");
+        System.out.println(shopping_list);
+        System.out.println(shopping_list.size());
+        shopping_list.add("Banana");
+
+
+
+    }
+}
+
